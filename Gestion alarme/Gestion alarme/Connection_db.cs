5645970f25d57240b56bf335de90afa9831afe0b
RedAXELight <Alexandre.baseia@cpnv.ch>
2018-12-06 10:33:50 +0100

    Create Connection_db.cs
    mise en place de la classe pour la connection avec la bd

---
+++ Gestion alarme/Gestion alarme/Connection_db.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Web;
+
+namespace Gestion_alarme
+{
+    class Connection_db
+    {
+    }
+}
