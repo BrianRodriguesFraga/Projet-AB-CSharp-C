a9d69b51068ed12f0c1f217a7b240c846be2a0a9
RedAXELight <Alexandre.baseia@cpnv.ch>
2018-12-11 09:17:16 +0100

    Connection
    Mise en place de la connection pour le base de données

--- Gestion alarme/Gestion alarme/Connection_db.cs
+++ Gestion alarme/Gestion alarme/Connection_db.cs
@@ -3,12 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
 using System.Web;
 
 namespace Gestion_alarme
 {
+    /// <summary>
+    /// This class is made for initialize and do some interactions with the database
+    /// </summary>
     class Connection_db
     {
+        MySqlConnection connectionString = new MySqlConnection("server=localhost;user id=root;database=alarme");
+
+        public void OpenConnection()
+        {
+            connectionString.Open();
+        }
+
+        public void CloseConnection()
+        {
+            connectionString.Close();
+        }
+
     }
+
+
+
+
 }
